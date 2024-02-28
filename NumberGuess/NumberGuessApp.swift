cb129688b6fbfeb54aabe85d406ca944b108634d
Oncu <oohancan@gmail.com>
2024-02-28 03:26:25 +0300

    Initial Commit

---
+++ NumberGuess/NumberGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberGuessApp.swift
+//  NumberGuess
+//
+//  Created by Oncu Can on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NumberGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
