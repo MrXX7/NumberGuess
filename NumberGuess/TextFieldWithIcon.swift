934149d644c69abad177d1464d5fd4f78c2db9c4
Oncu <oohancan@gmail.com>
2024-03-02 15:54:51 +0300

    Added X button to clear text in TextFieldWithIcon

--- NumberGuess/TextFieldWithIcon.swift
+++ NumberGuess/TextFieldWithIcon.swift
@@ -19,34 +19,46 @@ struct TextFieldWithIcon: View {
                     .font(.title)
                     .padding(.leading, 1)
                 
-                TextField("Enter your guess", text: $guess)
-                    .font(.title2)
-                    .foregroundColor(.black)
-                    .padding(.vertical, 8)
-                    .keyboardType(.numberPad) // Klavye sadece rakam tuşlarını içerecek şekilde ayarlandı
-                    .onChange(of: guess) { newValue in
-                        // Tahmin yapılmadan önce geri bildirimi sıfırla
-                        feedback = Array(repeating: .gray, count: 4)
-                        
-                        // Eğer girilen metnin uzunluğu 5 veya daha fazlaysa, son karakteri kaldır
-                        if newValue.count >= 5 {
-                            guess = String(newValue.dropLast())
+                HStack {
+                    TextField("Enter your guess", text: $guess)
+                        .font(.title2)
+                        .foregroundColor(.black)
+                        .padding(.vertical, 8)
+                        .keyboardType(.numberPad) // Klavye sadece rakam tuşlarını içerecek şekilde ayarlandı
+                        .onChange(of: guess) { newValue in
+                            // Tahmin yapılmadan önce geri bildirimi sıfırla
+                            feedback = Array(repeating: .gray, count: 4)
+                            
+                            // Eğer girilen metnin uzunluğu 5 veya daha fazlaysa, son karakteri kaldır
+                            if newValue.count >= 5 {
+                                guess = String(newValue.dropLast())
+                            }
                         }
+                        .onTapGesture {
+                            // Herhangi bir yere dokunulduğunda klavye kapanacak
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        }
+                    
+                    Button(action: {
+                        // Metni sıfırla
+                        guess = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.gray)
                     }
-                    .onTapGesture {
-                        // Herhangi bir yere dokunulduğunda klavye kapanacak
-                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                    }
-
+                    .padding(.trailing, 8)
+                }
             }
-        .padding(.horizontal, 15)
-        .background(
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.white)
-                .shadow(color: .black.opacity(0.1), radius: 3, x: 0, y: 2)
-            )}
+            .padding(.horizontal, 15)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.white)
+                    .shadow(color: .black.opacity(0.1), radius: 3, x: 0, y: 2)
+            )
+        }
     }
 }
+
 
 
 
