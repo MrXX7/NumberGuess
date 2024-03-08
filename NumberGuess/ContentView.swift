89802a77c615baef22707e033f5295679bcb5ade
Oncu <oohancan@gmail.com>
2024-03-08 13:49:42 +0300

    Updated the display conditions for the text messages to show "Code cracked! 🎉" when the user successfully cracks the code and to show "Crack the Code:" otherwise. Also added a message to display when the user runs out of attempts. Improved animations for better user experience.

--- NumberGuess/ContentView.swift
+++ NumberGuess/ContentView.swift
@@ -11,26 +11,49 @@ struct ContentView: View {
     @State private var guess = ""
     @State private var feedback = [Color.gray, Color.gray, Color.gray, Color.gray]
     @State private var target = generateUniqueCode()
-    
-//    String(format: "%04d", Int.random(in: 0..<10000))
     @State private var remainingAttempts = 6
+    @State private var showCongratulations = false
     
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color.purple, Color.black]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
+            
             VStack {
                 Spacer()
-                Text("Crack the Code:")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.top, 50)
+                
+                if feedback.allSatisfy({ $0 == Color.green }) {
+                    Text("Code cracked! 🎉")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.top, 20)
+                        .opacity(showCongratulations ? 1 : 0)
+                        .scaleEffect(showCongratulations ? 1 : 0.1)
+                        .animation(.easeInOut(duration: 1))
+                        .onAppear {
+                            withAnimation {
+                                showCongratulations.toggle()
+                            }
+                        }
+                } else if remainingAttempts == 0 {
+                    Text("You've used all your attempts. The correct answer was \(target).")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.top, 20)
+                } else {
+                    Text("Crack the Code:")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.top, 50)
+                }
+                
+                
                 
                 feedbackCircles(feedback: feedback, guessedNumbers: Array(guess).compactMap { Int(String($0)) })
                 
-                TextFieldWithIcon(guess: $guess, feedback: $feedback).frame(width: UIScreen.main.bounds.width * 0.65)
-                
+                TextFieldWithIcon(guess: $guess, feedback: $feedback)
+                    .frame(width: UIScreen.main.bounds.width * 0.65)
                     .padding(.top, 10)
                 
                 HStack(spacing: 100) {
@@ -40,7 +63,6 @@ struct ContentView: View {
                             remainingAttempts -= 1
                         }
                     }
-
                     .buttonStyle(CustomButtonStyle())
                     
                     Button("Reset") {
@@ -59,20 +81,7 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                if feedback.allSatisfy({ $0 == Color.green }) {
-                    Text("Congratulations! 🎉")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                        .padding(.top, 20)
-
-                } else if remainingAttempts == 0 {
-                    Text("You've used all your attempts. The correct answer was \(target).")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding(.top, 20)
-                }
                 
-                Spacer()
                 
                 Text("© 2024 by Oncu Can. All rights reserved.")
                     .font(.footnote)
@@ -81,10 +90,11 @@ struct ContentView: View {
             }
         }
         .onTapGesture {
-                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-            }
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        }
     }
 }
+
 
 
 
